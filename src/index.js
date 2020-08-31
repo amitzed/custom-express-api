86fe436fb4d2d39376cf423c0d0877a809aaddb4
amitzed <amitzmn@gmail.com>
2020-08-31 18:16:46 -0400

    Using Mongoose, wired up so that it communicates with MongoDB cloud -- data was successfully communicated

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+require('./models/User');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
 
 const authRoutes = require('./routes/authRoutes');
 
+app.use(bodyParser.json());
 app.use(authRoutes);
 
 const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.ptzkt.mongodb.net/<dbname>?retryWrites=true&w=majority';
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = new User({ email, password });
+
+  await user.save();
+
   res.send('A Post Request')
 });
 
